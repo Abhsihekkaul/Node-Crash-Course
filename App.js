ae90eeb2437d437166689fd917982611489fdf6b
Abhishek kaul <Abhishekkaul32@gmail.com>
2025-01-16 13:37:06 +0530

    Replicated the same using the express and also learned about the redirect and use of middlewares

--- App.js
+++ App.js
@@ -1,42 +1,44 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+// No need to require the 'fs' module here as Express handles file routing efficiently.
 
-    // Set header content type
-    res.setHeader('Content-Type', 'text/html');
-
-    // Creating a structure for multiple URLs
-    let path = './views/';
-    switch(req.url){
-        case '/':
-            path += 'index.html';
-            res.statusCode = 200;
-            break;
-        case '/about':
-            path += 'about.html';
-            res.statusCode = 200;
-            break;
-        default:
-            path += '404.html';
-            res.statusCode = 404;
-            break;
-    }
-
-    // Read file
-    fs.readFile(path, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end();
-        } else {
-            // res.write(data);
-            // Directly send the data in the response
-            res.end(data);
-        }
-    });
+// Creating a route for the home page
+app.get('/', (req, res) => {
+    // Use sendFile to serve static HTML files. Always specify the root directory to avoid errors.
+    res.sendFile("./views/index.html", { root: __dirname });
 });
 
-server.listen(8000, () => {
+
+
+// Creating a route for the About page
+app.get('/about', (req, res) => {
+    // Similar to the home route, serve the About HTML file.
+    res.sendFile("./views/about.html", { root: __dirname });
+});
+
+// using the redirect function
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+// Creating a route for the Error page
+// app.get('/error', (req, res) => {
+//     // Fixed: Use sendFile instead of send to serve the 404 HTML file.
+//     res.sendFile("./views/404.html", { root: __dirname });
+// });
+
+// Creating a route for the Error page
+// but not like the way i did above what i will do i will create a middleware that will handle the error page
+// everytime any user visit a page that doesn't exist it will redirect to the error page
+
+// Middleware to handle 404 errors
+app.use((req, res) => {
+    // Fixed: Use sendFile instead of send to serve the 404 HTML file.
+    res.sendFile("./views/404.html", { root: __dirname });
+});
+
+// Starting the server on port 8000
+app.listen(8000, () => {
     console.log("The server is running on port 8000");
 });
