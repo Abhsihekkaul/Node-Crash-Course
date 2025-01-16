702fbc2969444b8fa8870241a53dbe90a06902b1
Abhishek kaul <Abhishekkaul32@gmail.com>
2025-01-16 13:40:27 +0530

    cleaning the code which i have written so far

--- App.js
+++ App.js
@@ -1,44 +1,32 @@
 const express = require('express');
 const app = express();
 
-// No need to require the 'fs' module here as Express handles file routing efficiently.
-
-// Creating a route for the home page
+// getting the index.html file
 app.get('/', (req, res) => {
-    // Use sendFile to serve static HTML files. Always specify the root directory to avoid errors.
     res.sendFile("./views/index.html", { root: __dirname });
 });
 
 
-
-// Creating a route for the About page
 app.get('/about', (req, res) => {
-    // Similar to the home route, serve the About HTML file.
     res.sendFile("./views/about.html", { root: __dirname });
 });
-
-// using the redirect function
+// added the redirect for the search mapping to the about page
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
 });
 
-// Creating a route for the Error page
-// app.get('/error', (req, res) => {
-//     // Fixed: Use sendFile instead of send to serve the 404 HTML file.
-//     res.sendFile("./views/404.html", { root: __dirname });
-// });
 
-// Creating a route for the Error page
-// but not like the way i did above what i will do i will create a middleware that will handle the error page
-// everytime any user visit a page that doesn't exist it will redirect to the error page
 
-// Middleware to handle 404 errors
+
+// using a middleware for the errors
 app.use((req, res) => {
-    // Fixed: Use sendFile instead of send to serve the 404 HTML file.
     res.sendFile("./views/404.html", { root: __dirname });
 });
 
-// Starting the server on port 8000
+
+
+
+// Listening to the port 8000
 app.listen(8000, () => {
     console.log("The server is running on port 8000");
 });
